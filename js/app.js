d7f1f32d15b838ddd1098a1b6549dde7c5d79e21
Chengsu Chen <cchengsu@gmail.com>
2015-12-06 16:23:27 -0800

    What We Do and mutli-page buttons, and JS file modified

--- js/app.js
+++ js/app.js
@@ -1,1 +1,42 @@
-var myApp = angular.module('myApp', ['ui.router']);
+// Create app
+var myApp = angular.module('myApp', ['ui.router'])
+// Configure app
+myApp.config(function($stateProvider) {
+		$stateProvider
+		.state('atwork', {
+			url:'../templates/atwork',
+			templateUrl: '../templates/atwork.html',
+			controller: 'atworkController',
+		})
+
+		$stateProvider
+		.state('comed', {
+			url:'/community_education',
+			templateUrl: '../templates/HV/comed.html',
+			controller: 'ComEdController',
+		})
+
+		$stateProvider
+		.state('HC', {
+			url:'/healthcenters',
+			templateUrl: '../templates/HV/healthcenters.html',
+			controller: 'HCController',
+		})
+
+})
+
+	// About page controller: define $scope.about as a string
+
+		myApp.controller('atworkController', function($scope){
+			$scope.AtWork = "info"
+		})
+
+	// Content controller: define $scope.url as an image
+		myApp.controller('ComEdController', function($scope){
+		    $scope.ComEd= "info"
+		})
+
+		myApp.controller('HCController', function($scope){
+			$scope.HC = "info"
+		})
+
